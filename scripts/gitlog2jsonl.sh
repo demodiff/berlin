cd3195e22dfd3380c2c15ddfc911c183733ceefa
Lennart <lennart@lnrt.de>
2022-10-02 12:49:48 +0200

    data: remove obsolte files

--- scripts/gitlog2jsonl.sh
+++ scripts/gitlog2jsonl.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-for i in $(git log --format='%H'); do
-  git show $i:data/results.jsonl > ../data/jsonl/$(git show -s --format=%cd --date=format:'%Y%m%d-%H%M%S' $i)-$i.jsonl
+for i in $(git log --format='%H' -- data/results.jsonl); do
+  git show $i:data/results.jsonl > data/jsonl/$(git show -s --format=%cd --date=format:'%Y%m%d-%H%M%S' $i)-$i.jsonl
 done
